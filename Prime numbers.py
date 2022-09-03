a9818980266f0560636d0ec8e7f5ae09103ed9ac
Sushikiransk777 <112744699+Sushikiransk777@users.noreply.github.com>
2022-09-03 20:30:46 +0530

    commit

---
+++ Prime numbers.py
@@ -0,0 +1,9 @@
+
+print("Prime numbers between 1 and 200 are:")
+for num in range(1, 200 + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
